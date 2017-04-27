85481f7ad885046716c554cacc0a0ef3a59264cf
pntruongan <pntruongan@gentoopc>
2017-04-27 16:48:30 +0700

    Fix Problem edit page just reload it self every time Turn on xsrf protection by default clean up various bits.

--- application/controllers/Problems.php
+++ application/controllers/Problems.php
@@ -178,9 +178,11 @@ class Problems extends CI_Controller
 		if ( ! is_numeric($problem_id) || $problem_id < 1 || $problem_id > $data['description_assignment']['problems'])
 			show_404();
 
-		$this->form_validation->set_rules('text', 'text' ,''); /* todo: xss clean */
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('text', 'text' ,'required'); /* todo: xss clean */
 		if ($this->form_validation->run())
 		{
+
 			$this->assignment_model->save_problem_description($assignment_id, $problem_id, $this->input->post('text'), $ext);
 			redirect('problems/'.$assignment_id.'/'.$problem_id);
 		}
