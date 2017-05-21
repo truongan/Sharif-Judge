e6db63b9e15189f93cbda00151f4f1b51297b153
truongan@dell-xps-13-9343 <truongan@dell-xps-13-9343>
2017-05-21 16:08:05 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
-
-#git clone  'https://github.com/truongan/Sharif-Judge' . 
 usage(){
 cat << EOF
-usage: $0 [-i install_dir] [-o public_dir] -u db_user -p db_password base_url 
+usage: $0 [-i install_dir] [-o public_dir] -u db_user -p db_password [-n site_name] [base_url]
 
 base url will be set in config.php
 
 OPTIONS:
 	-h show this message
 	-i install directory, default to current working directory
-	-o public directory to put index.php, default to be the public 
+	-o public directory to put index.php, default to be the public
 	   directory in the same directory as install directory
 	-u database username
 	-p database password
 	-d database name
+	-n site name to display on top bar and construct base url for uit classes
 EOF
 }
 
@@ -25,27 +24,33 @@ db_user=''
 db=''
 db_password=''
 base_url=''
-
-while getopts "hi:o:u:p:d:" ops ; do
-	case "${ops}" in 
+site_name=''
+while getopts "hi:o:u:p:d:n:" ops ; do
+	case "${ops}" in
 		h)	usage ;;
 		i)	install=${OPTARG};;
 		o)	public=${OPTARg};;
 		u)	db_user=${OPTARG};;
 		p)	db_password=${OPTARG};;
 		d)	db=${OPTARG};;
+		n)  site_name=${OPTARG};;
 		*)	usage; exit 1;;
 	esac
 done
 shift $((OPTIND-1))
 
-base_url=$1
+if [ "$site_name" = "" ]; then
+	base_url=$1
+	site_name="Wecode Judge"
+else
+	base_url="https://khmt.uit.edu.vn/laptrinh/`echo $site_name | tr '[:upper:]' '[:lower:]'`/"
+fi
 
 if [ "$db_user" = "" ]; then
 	usage; exit 1
 fi
 
-if [ "$public" = "" ]; then 
+if [ "$public" = "" ]; then
 	public="$install/../public_html"
 fi
 
@@ -53,18 +58,20 @@ if [ "$db" = '' ]; then
 	db="$db_user"
 fi
 
-cat << EOF 
+cat << EOF
 "install=$install"
 "public=$public"
 "db_user=$db_user"
 "db=$db"
 "db_password=$db_password"
 "base_url=$base_url"
+"site_name=$site_name"
 EOF
 
 cd $install
-git clone  'https://github.com/truongan/Sharif-Judge' . 
-git checkout wecode
+git clone  'https://github.com/truongan/wecode-judge' .
+git checkout working-updateci 
+
 
 cd $public
 rm index.html index.php
@@ -75,12 +82,19 @@ sed -i "s@application_folder = 'application'@application_folder = '$install/appl
 
 cd $install/application/config
 cp config.php.example config.php
-cp database.php.example database.php 
+cp database.php.example database.php
+mkdir $install/application/session/
 
 echo sed -i "s@base_url'] = ''@base_url'] = '$base_url'@g" config.php
 sed -i "s@base_url'] = ''@base_url'] = '$base_url'@g" config.php
 sed -i "s@index_page'] = 'index.php'@index_page'] = ''@g" config.php
+sed -i "s@cookie_path']		= '@cookie_path']		= '/$site_name/'@g" config.php
 
+pwd
 sed -i "s/homestead/$db_user/g" database.php
 sed -i "s/secret/$db_password/g" database.php
 sed -i "s/sharif/$db/g" database.php
+
+cd $install/application/controllers
+echo sed -i "s@_sitenametobereplace_@$site_name@g" Install.php
+sed -i "s@_sitenametobereplace_@$site_name@g" Install.php
