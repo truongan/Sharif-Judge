bfdc632a160c7144dcaeff910c9d1daac21de22c
pntruongan <pntruongan@gentoopc>
2017-06-15 02:04:34 +0700

    move script and style out of submit.twig file

--- assets/js/submit_page.js
+++ assets/js/submit_page.js
@@ -38,32 +38,46 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
-var editor = ace.edit("editor");
 
-var theme = getCookie("code_theme");
-if (theme == "") theme = "dawn";
+$(document).ready(function(){
+    $("select#problems").change(function(){
+        var v = $(this).val();
+        $('select#languages').empty();
+        //$('<option value="0" selected="selected">-- Select Language --</option>').appendTo('select#languages');
+        if (v==0)
+            return;
+        for (var i=0;i<shj.p[v].length;i++)
+            $('<option value="'+shj.p[v][i]+'">'+shj.p[v][i]+'</option>').appendTo('select#languages');
+        $("#problem_link").attr('href', "/problems/{{ user.selected_assignment.id }}/" + $(this).val());
 
-editor.setTheme("ace/theme/" + theme);
-$("#theme").val(theme);
+    });
 
-editor.session.setMode("ace/mode/c_cpp");
-$("form").submit(function(){
-	$("textarea").val(editor.getSession().getValue());
-});
+    var editor = ace.edit("editor");
 
-$("select[name=language]").change(function(){
+    var theme = getCookie("code_theme");
+    if (theme == "") theme = "dawn";
 
-	var lang_to_mode = {"C++":"c_cpp"
-		, Java:"java"
-		, "Python 2":"python"
-		, "Python 3":"python"
-	};
-	editor.session.setMode("ace/mode/" + lang_to_mode[$(this).val()]);
-});
+    editor.setTheme("ace/theme/" + theme);
+    $("#theme").val(theme);
 
-$("#theme").change(function(){
-    console.log("SHIT");
-	t = $(this).val();
-	editor.setTheme("ace/theme/" + t);
-	setCookie('code_theme', t, 30);
+    editor.session.setMode("ace/mode/c_cpp");
+    $("form").submit(function(){
+    	$("textarea").val(editor.getSession().getValue());
+    });
+
+    $("select[name=language]").change(function(){
+
+    	var lang_to_mode = {"C++":"c_cpp"
+    		, Java:"java"
+    		, "Python 2":"python"
+    		, "Python 3":"python"
+    	};
+    	editor.session.setMode("ace/mode/" + lang_to_mode[$(this).val()]);
+    });
+
+    $("#theme").change(function(){
+    	t = $(this).val();
+    	editor.setTheme("ace/theme/" + t);
+    	setCookie('code_theme', t, 30);
+    });
 });
