a11d7f3287b0c5e9c0bddac7ea8c0322193869ec
truongan@dell-xps-13-9343 <truongan@dell-xps-13-9343>
2018-03-16 19:37:02 +0700

    FIX: html editor accessible without logging in

--- application/controllers/Htmleditor.php
+++ application/controllers/Htmleditor.php
@@ -8,10 +8,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Htmleditor extends CI_Controller
 {
+    public function __construct()
+	{
+		parent::__construct();
+		if ( ! $this->user->logged_in()) // if not logged in
+			redirect('login');
+		if ( $this->user->level <= 2) // permission denied
+			show_404();
+	}
     public function index(){
         $this->twig->display('pages/htmleditor.twig', array());
     }
-
 }
 
 ?>
