ef0275b1c3858c2c4fcaaa1e972baf69fd3d090f
truongan <truongan2005@gmail.com>
2019-05-06 02:04:08 +0700

    use queue table as mutex

---
+++ application/migrations/20190505150459_multi_queue.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Multi_queue extends CI_Migration {
+    public function up(){
+        $this->dbforge->add_column('queue', array(
+            'process_id' => array('type' =>'INT', 'constraint' => 11, 'unsigned' => TRUE, 'null' => TRUE)
+        ));
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_column('languages', 'sorting');
+        //Can not be downed. 
+        //This is the first imgrations. 
+    }
+}
+?>
