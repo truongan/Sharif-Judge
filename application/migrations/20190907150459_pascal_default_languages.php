88dcc23f7d6031ec2dda46a059fdf2cf261f28d1
truongan <truongan2005@gmail.com>
2019-09-07 03:30:24 +0700

    fix: pycache folder still in jail

---
+++ application/migrations/20190907150459_pascal_default_languages.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_pascal_default_languages extends CI_Migration {
+    public function up(){
+        $this->dbforge->add_column('queue', array(
+            'process_id' => array('type' =>'INT', 'constraint' => 11, 'unsigned' => TRUE, 'null' => TRUE)
+        ));
+        $this->db->insert('settings', array('shj_key' => 'concurent_queue_process', 'shj_value' => 2));
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_column('queue', 'process_id');
+    }
+}
+?>
