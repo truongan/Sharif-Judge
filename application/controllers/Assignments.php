25db4124b84ff8c65618f393d3d05db6f741cdc5
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2020-02-20 13:08:46 +0700

    abc

--- application/controllers/Assignments.php
+++ application/controllers/Assignments.php
@@ -112,8 +112,8 @@ class Assignments extends CI_Controller
 			foreach($all_assignments as $id => $ass){
 				if (isset($user['assignments'][$id])){
 					$c += 1;
-					$user['sum']->accepted += (int)$user['assignments'][$id]->accepted;
-					$user['sum']->total += (int)$user['assignments'][$id]->total;
+					$user['sum']->accepted =  (int)$user['sum']->accepted + (int)$user['assignments'][$id]->accepted;
+					$user['sum']->total = (int)$user['sum']->total + (int)($user['assignments'][$id]->total);
 				}
 			}
 
